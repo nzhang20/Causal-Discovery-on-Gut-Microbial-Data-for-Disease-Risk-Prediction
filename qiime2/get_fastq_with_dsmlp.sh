b32ac1a4a74d116c3455ee459306095a9d016c01
mpm2022 <mpacomendivil@ucsd.edu>
2025-02-20 23:21:42 -0800

    bash script for data download in dsmlp

---
+++ qiime2/get_fastq_with_dsmlp.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+SRA_PATH="$HOME/teams/a09/team1/kondo_et_al_2021/sra-toolkit/sratoolkit.3.2.0-ubuntu64/bin"
+
+#Requires prefecth and fasterq-dump from sra-toolkit
+if [[ !  -x "$SRA_PATH/prefetch" ]]; then
+    echo "prefetch could not be found. Please install the SRA Toolkit."
+    exit 1
+fi
+
+if [[ ! -x "$SRA_PATH/fasterq-dump" ]]; then
+    echo "fasterq-dump could not be found. Please install the SRA Toolkit."
+    exit 1
+fi
+
+#Insert CSV file with accession codes
+csv_file="sra-explorer.csv"
+
+if [[ ! -f "$csv_file" ]]; then
+    echo "The file $csv_file does not exist."
+    exit 1
+fi
+
+tail -n +2 "csv_file" | while IFS=',' read -r _ accession; do
+    #if [[ "$accession" == "accession" ]]; then
+    #    continue
+    #fi
+
+    #Get accession code
+    accession=$(echo "$accession" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//;s/"//g;s/'"'"'//g')
+    echo "Processing accession code: ${accession}"
+
+    #Use prefetch to download the SRA file
+    if "$SRA_PATH/prefetch" "$accession"; then
+        echo "Downloaded $accession successfully."
+
+        #Convert the downloaded SRA file to FASTQ format
+        if "$SRA_PATH/fasterq-dump" --split-files "$accession"; then
+            echo "Converted $accession to FASTQ format successfully."
+        else
+            echo "Failed to convert $accession to FASTQ format."
+        fi
+
+        #Remove the SRA file after conversion
+        rm "${accession}.sra" 2>/dev/null
+
+    else
+        echo "Failed to download $accession."
+    fi
+done < "$csv_file"
+
+echo "All accessions processed."
